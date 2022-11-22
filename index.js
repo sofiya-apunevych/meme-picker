d84e84ce91841babc587ec9f08da25a53b83bed3
sofiya-apunevych <62404390+sofiya-apunevych@users.noreply.github.com>
2022-11-22 19:41:53 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,119 +1,96 @@
-import { catsData } from '/data.js'
+const myEmojis = ["👩🏼‍💻", "☕️", "📚", "🏞", "⛸", "🐶", "🐱"]
 
-const emotionRadios = document.getElementById('emotion-radios')
-const getImageBtn = document.getElementById('get-image-btn')
-const gifsOnlyOption = document.getElementById('gifs-only-option')
-const memeModalInner = document.getElementById('meme-modal-inner')
-const memeModal = document.getElementById('meme-modal')
-const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
+const emojiContainer = document.getElementById("emoji-container")
+const emojiInput = document.getElementById("emoji-input")
+const pushBtn = document.getElementById("push-btn")
+const unshiftBtn = document.getElementById("unshift-btn")
+const popBtn = document.getElementById("pop-btn")
+const shiftBtn = document.getElementById("shift-btn")
 
-const main = document.getElementById("main")
-
-const head = document.getElementById("head")
-
-emotionRadios.addEventListener('change', highlightCheckedOption)
-
-getImageBtn.addEventListener('click', renderCat)
-
-function highlightCheckedOption(e){
-    const radios = document.getElementsByClassName('radio')
-    for (let radio of radios){
-        radio.classList.remove('highlight')
-    }
-    document.getElementById(e.target.id).parentElement.classList.add('highlight')
-}
-
-
-function renderCat(){
-    const catsObject = getSingleCatObject()
-   memeModalInner.innerHTML = ``
-
-    for(let catObject of catsObject){
-
-    memeModalInner.innerHTML +=  `
-        <img 
-        class="cat-img" 
-        src="./images/${catObject.image}"
-        alt="${catObject.alt}"
-        >
-        `
-    memeModal.style.display = 'flex'
+function renderEmojis() {
+    emojiContainer.innerHTML = ""
+    for (let i = 0; i < myEmojis.length; i++) {
+        const emoji = document.createElement('span')
+        emoji.textContent = myEmojis[i]
+        emojiContainer.append(emoji)
     }
 }
 
-function getSingleCatObject(){
-  
-    const catsArray = getMatchingCatsArray()
+renderEmojis()
+
+pushBtn.addEventListener("click", function(){
+    if (emojiInput.value) {
+        myEmojis.push(emojiInput.value)
+        emojiInput.value = ""
+        renderEmojis()
+    }
+})
+
+unshiftBtn.addEventListener("click", function(){
+    if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value)
+        emojiInput.value = ""
+        renderEmojis()
+    }
+})
+
+popBtn.addEventListener("click", function() {
+    myEmojis.pop()
+    renderEmojis()
+})
+
+shiftBtn.addEventListener("click", function() {
+    myEmojis.shift()
+    renderEmojis()
+})
+
+
+
+
+
+let secretEm = document.getElementById("secret")
+let jobEm = document.getElementById("job")
+let friendEm = document.getElementById("friend")
+
+let rutineBtn = document.getElementById("rutine-btn")
+
+let person = {
+    secretEmoji: "👶 👮 🕵️‍♀️",
+    jobEmoji: "👨‍🔧 👩‍🔬",
+    friendEmoji: "👨🏻‍🦱 👩🏻‍🦰",
+};
+
+localStorage.setItem("profile", JSON.stringify(person));
+
+let storageProfileString = localStorage.getItem("profile");
+console.log("String saved in LocalStorage", storageProfileString);
+
+let savedPerson = JSON.parse(storageProfileString);
+console.log("Person object:", savedPerson);
+console.log("Person's name:", savedPerson.friendEmoji);
+
+
+secretEm.addEventListener("click", function(){
+    emojiContainer.innerText = ""
+    emojiContainer.innerText = savedPerson.secretEmoji
+})
+
+jobEm.addEventListener("click", function(){
+    emojiContainer.innerText = ""
+    emojiContainer.innerText = savedPerson.jobEmoji
+})
+
+friendEm.addEventListener("click", function(){
+    emojiContainer.innerText = ""
+   emojiContainer.innerText = savedPerson.friendEmoji 
+})
+
+rutineBtn.addEventListener("click", function(){
+    emojiContainer.innerHTML = ""
+    for (let i = 0; i < myEmojis.length; i++) {
+        const emoji = document.createElement('span')
+        emoji.textContent = myEmojis[i]
+        emojiContainer.append(emoji)
+    }
     
-         const randomNumberOne = Math.floor(Math.random() * catsArray.length)
-         const randomNumberTwo = Math.floor(Math.random() * catsArray.length)
-    
-    if (catsArray.length === 1){
-        return catsArray[0]
-    }
-    else if(catsArray.length >= 2 && randomNumberOne!==randomNumberTwo){
-        return [catsArray[randomNumberOne], catsArray[randomNumberTwo]] 
-         }
-}
-
-function getMatchingCatsArray(){     
-    if(document.querySelector('input[type="radio"]:checked')){
-        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
-        const isGif = gifsOnlyOption.checked
-        
-        const matchingCatsArray = catsData.filter(function(cat){
-            
-            if(isGif){
-                return cat.emotionTags.includes(selectedEmotion) && cat.isGif 
-            }
-            else{
-                return cat.emotionTags.includes(selectedEmotion)
-            }            
-        })
-        return matchingCatsArray 
-    }  
-}
-
-function getEmotionsArray(cats){
-    const emotionsArray = []    
-    for (let cat of cats){
-        for (let emotion of cat.emotionTags){
-            if (!emotionsArray.includes(emotion)){
-                emotionsArray.push(emotion)
-            }
-        }
-    }
-    return emotionsArray
-}
-
-function renderEmotionsRadios(cats){
-        
-    let radioItems = ` `
-    const emotions = getEmotionsArray(cats)
-    for (let emotion of emotions){
-        radioItems += `
-        <div class="radio">
-            <label for="${emotion}">${emotion}</label>
-            <input
-            type="radio"
-            id="${emotion}"
-            value="${emotion}"
-            name="emotions"
-            >
-        </div>`
-    }
-    emotionRadios.innerHTML = radioItems
-}
-
-renderEmotionsRadios(catsData)
-
-
-document.body.onclick = function(event) {
-   if (event.target == head ) {
-       memeModal.style.display = "none"
-    }
-   if (event.target == main ) {
-       memeModal.style.display = "none"
-    }
-}
-
+})
